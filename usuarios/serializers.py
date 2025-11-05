c83cc39b2ac1c62d2fc34846a22817a0db279516
Prácticas <alexdaw995@gmail.com>
2025-11-05 10:30:18 +0100

    Añadir api de registro de usuarios (serializers, resgisterView)

---
+++ usuarios/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'phone', 'bio', 'is_professional', 'password']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            phone=validated_data.get('phone', ''),
+            bio=validated_data.get('bio', ''),
+            is_professional=validated_data.get('is_professional', False),
+            password=validated_data['password']
+        )
+        return user
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        # Validamos la información enviada
+        serializer.is_valid(raise_exception=True)
+
+        # Creamos el usuario
+        serializer.save()
+
+        return Response(
+            {"message": "Usuario creado correctamente"},
+            status=status.HTTP_201_CREATED
+        )
