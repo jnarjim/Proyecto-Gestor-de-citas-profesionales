57a37c778000883ef93a9009206e7093fbb3b2dc
Jorge Naranjo <jnarjim817@fp.iesromerovargas.com>
2025-12-15 11:08:56 +0100

    Preparado

--- gestor_citas/settings.py
+++ gestor_citas/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret-key-change-in-production
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
